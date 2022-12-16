5bbd6a7bfdfd9706bd85749eac8c17523dd7c065
Quyen <queen@Quyen.local>
2022-12-16 15:44:38 +0700

    update voc.py

--- yolox/data/datasets/voc.py
+++ yolox/data/datasets/voc.py
@@ -223,8 +223,7 @@ class VOCDetection(Dataset):
     def load_image(self, index):
         img_id = self.ids[index]
         img = cv2.imread(self._imgpath % img_id, cv2.IMREAD_COLOR)
-        assert img is not None, f"file named {self._imgpath % img_id} not found"
-        img[:, :, 0] = img[:, :, 0]*1.5
+        assert img is not None, f"file named {self._imgpath % img_id} not found"        
         
         return img
 
